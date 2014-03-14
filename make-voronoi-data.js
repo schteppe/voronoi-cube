8709b713168ff318ff572c16f20af19543531dbd
Stefan Hedman <schteppe@gmail.com>
2014-03-14 15:49:01 +0100

    cleanup

--- make-voronoi-data.js
+++ make-voronoi-data.js
@@ -2,7 +2,7 @@ var exec = require('child_process').exec;
 var fs = require('fs');
 var path = require('path');
 
-var template = '{"id":%i,"position":{"x":%x,"y":%y,"z":%z},"vertices":"%p","faces":"%t","normals":"%l","volume":%v},';
+var template = '{"id":%i,"position":{"x":%x,"y":%y,"z":%z},"vertices":"%p","faces":"%t","normals":"%l","volume":%v}';
 
 // Generate particle data
 var data = '';
@@ -18,12 +18,34 @@ exec(cmd,function(err,stdout,stderr){
 	if(err)
 		throw err;
 
+	// voro++ creates a .vol file
 	var data = fs.readFileSync(path.join(__dirname,'particles.txt.vol'));
-	fs.writeFileSync(path.join(__dirname,'volumes.js'),'volumes=['+data+']');
+	var json = JSON.parse('['+data.toString().replace(/\n/g,',').replace(/,$/,'')+']');
+
+	// Make each vector array nicer
+	for(var i=0; i<json.length; i++){
+		json[i].faces = parseVectorArray(json[i].faces, parseInt);
+		json[i].vertices = parseVectorArray(json[i].vertices,parseFloat);
+	}
+
+	var outPath = path.join(__dirname,'volumes.js');
+	fs.writeFileSync(outPath,'volumes='+JSON.stringify(json,2,2));
 
 	// Remove temp files
 	fs.unlinkSync(path.join(__dirname,'particles.txt'));
 	fs.unlinkSync(path.join(__dirname,'particles.txt.vol'));
 
-	console.log("Created volumes.js")
+	console.log("Created "+outPath)
 });
+
+
+// Parses an array "(1,2,3) (1,2,3) (1,2,3)" to [[1,2,3],[1,2,3],[1,2,3]]
+function parseVectorArray(verts,parse){
+    var verts = verts.split(' ');
+    for(var i=0; i<verts.length; i++){
+        verts[i] = verts[i].replace(/[\(\)]/g,'').split(',');
+        for(var j=0; j<verts[i].length; j++)
+            verts[i][j] = parse(verts[i][j]);
+    }
+    return verts;
+}
